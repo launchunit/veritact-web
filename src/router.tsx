7d03acd575639ff8f2b79e2a48910eb649502020
Karan Sakhuja <karan.sakhuja@outlook.com>
2026-02-23 21:51:48 -0500

    More fixes

--- src/router.tsx
+++ src/router.tsx
@@ -2,19 +2,17 @@ import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 
 export function getRouter() {
-  const router = createTanStackRouter({
+  return createTanStackRouter({
     routeTree,
 
     scrollRestoration: true,
     defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
   });
-
-  return router;
 }
 
 declare module '@tanstack/react-router' {
-  // biome-ignore lint/nursery/useConsistentTypeDefinitions Interface required here
+  // biome-ignore lint/nursery/useConsistentTypeDefinitions: Interface required here
   interface Register {
     router: ReturnType<typeof getRouter>;
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import tailwindcss from '@tailwindcss/vite';
+import tailwindCSS from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
@@ -7,15 +7,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { host: true },
+  envDir: false, // Do not load ".env" files
   plugins: [
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
+    tsconfigPaths(),
     tanstackStart(),
     viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
+      // https://react.dev/learn/react-compiler
+      babel: { plugins: ['babel-plugin-react-compiler'] },
     }),
+    tailwindCSS(),
   ],
 });
